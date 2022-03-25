91a0e35b72f4fffbb54abba64a2d92c44acf1ab0
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-25 23:37:09 +0530

    Update skills_controller.js

--- src/server/skills/skills_controller.js
+++ src/server/skills/skills_controller.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var PORT;
 var Url;
 var Skills = require("./skills_schema");
-mongoose.connect(Url, {
+mongoose.connect(PORT, {
     /*  mongodb://127.0.0.1:27017/portfolio 
         mongodb+srv://vipul:<password>@cluster0.3v2rv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
         process.env.PORT   `Url`
