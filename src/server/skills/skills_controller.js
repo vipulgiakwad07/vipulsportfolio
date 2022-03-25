3f9d2f894f64ddcac7e2e39b22634aa6aae01bb2
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-25 19:48:53 +0530

    Update skills_controller.js

--- src/server/skills/skills_controller.js
+++ src/server/skills/skills_controller.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require("mongoose");
 
 var Skills = require("./skills_schema");
-mongoose.connect(`https://vipulsportfolio.herokuapp.com`, {
+mongoose.connect(`https://vipulsportfolio.herokuapp.com/`, {
     /*  mongodb://127.0.0.1:27017/portfolio/ */
     useNewUrlParser: true,
     useUnifiedTopology: true,
