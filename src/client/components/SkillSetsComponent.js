4510d2722c85d06b187d0b4235f16302cb975fdd
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-27 14:53:08 +0530

    Update SkillSetsComponent.js

--- src/client/components/SkillSetsComponent.js
+++ src/client/components/SkillSetsComponent.js
@@ -53,6 +53,15 @@ class SkillSetsComponent extends React.Component {
             <div className={'skills-parent-container'}>
                 {this.renderTechStacks(results)}
                 <div className={'skills-mobile-gap'}></div>
+                 
+                          <div class="progress">
+                         <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="50"aria-valuemin="0" aria-valuemax="100" style="width:50%">          
+                            50% Complete (info)
+                            </div>
+                          </div>
+
+                      
+
               <table>
                <tc>
                 <tr><div className={'skill-round'}>react </div></tr>
