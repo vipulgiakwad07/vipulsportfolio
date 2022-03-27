71bdec2fe7bfdf6b7e469b05005e2f9fe4b1a7df
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-27 15:16:09 +0530

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -70,13 +70,7 @@ app.get('*', (req, res) => {
             
             
             
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
-           <!-- jQuery library -->
-           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
-           <!-- Latest compiled JavaScript -->
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+          
        
        
        
