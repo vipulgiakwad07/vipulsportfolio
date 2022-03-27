969a0b3287d035945c7519b8fa66feb5b1319231
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-27 14:51:33 +0530

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -66,7 +66,21 @@ app.get('*', (req, res) => {
             <link rel="canonical" href="${link}"/>
             <link rel="shortcut icon" type="image/png" href="${favicon}"/>
             <link rel="apple-touch-icon-precomposed" href="${thumb}"/>
-        </head>
+            
+            
+            
+            
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+           <!-- jQuery library -->
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+           <!-- Latest compiled JavaScript -->
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+       
+       
+       
+       </head>
         <body>
             <div id="root">${markup}</div>
             <script src="client_bundle.js"></script>
