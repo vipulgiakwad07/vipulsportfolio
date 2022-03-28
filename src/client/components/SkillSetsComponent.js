3fc0e7f6e37915f417f203e4f8d6719b26727030
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-28 13:42:49 +0530

    Update SkillSetsComponent.js

--- src/client/components/SkillSetsComponent.js
+++ src/client/components/SkillSetsComponent.js
@@ -68,10 +68,10 @@ class SkillSetsComponent extends React.Component {
 
                                            <div className="">
                                              <h3 className="heading">Progress Bar</h3>
-                                             /*<Progressbar bgcolor="orange" progress='30'  height={30} />
+                                             { /*<Progressbar bgcolor="orange" progress='30'  height={30} />
                                               <Progressbar bgcolor="red" progress='60'  height={30} />
                                               <Progressbar bgcolor="#99ff66" progress='50'  height={30} />
-                                              <Progressbar bgcolor="#ff00ff" progress='85'  height={30} /> */
+                                              <Progressbar bgcolor="#ff00ff" progress='85'  height={30} /> */  }
                                               <Progressbar bgcolor="#99ccff" progress='95'  height={30} />
                                            </div>
                
