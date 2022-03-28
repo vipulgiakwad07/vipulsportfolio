f1e83e84eb723a9952a6cac4bcd6bb147a6c6cf8
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-28 13:58:48 +0530

    Update SkillSetsComponent.js

--- src/client/components/SkillSetsComponent.js
+++ src/client/components/SkillSetsComponent.js
@@ -66,13 +66,34 @@ class SkillSetsComponent extends React.Component {
                  
                    
 
-                                           <div className="">
-                                             <h3 className="heading">Progress Bar</h3>
-                                             { /*<Progressbar bgcolor="orange" progress='30'  height={30} />
-                                              <Progressbar bgcolor="red" progress='60'  height={30} />
-                                              <Progressbar bgcolor="#99ff66" progress='50'  height={30} />
-                                              <Progressbar bgcolor="#ff00ff" progress='85'  height={30} /> */  }
+                                           <div className=""> 
+                                              <div>
+                                              <h3 className="heading">Progress Bar</h3>
                                               <Progressbar bgcolor="#99ccff" progress='95'  height={30} />
+                                              </div>
+
+                                               <div>
+                                              <h3 className="heading">Progress Bar</h3>
+                                              <Progressbar bgcolor="#3dCFD3" progress='95'  height={30} />
+                                              </div>
+
+                                              
+                                              <div>
+                                              <h3 className="heading">Progress Bar</h3>
+                                              <Progressbar bgcolor="#3dCFD3" progress='95'  height={30} />
+                                              </div>
+
+                                               <div>
+                                              <h3 className="heading">Progress Bar</h3>
+                                              <Progressbar bgcolor="#3dCFD3" progress='95'  height={30} />
+                                              </div>
+
+                                              
+                                               <div>
+                                              <h3 className="heading">Progress Bar</h3>
+                                              <Progressbar bgcolor="#3dCFD3" progress='95'  height={30} />
+                                              </div>
+                                             
                                            </div>
                
                       
