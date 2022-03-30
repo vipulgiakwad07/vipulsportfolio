3eb2136859f94ae90d2ab62378156d7c3fba6316
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-03-30 18:12:04 +0530

    Update constants.js

--- src/client/actions/constants.js
+++ src/client/actions/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://vipulsportfolio.herokuapp.com/';  {/*9002 ,27017    http://localhost:9002*/}
+export const BASE_URL = ' mongodb+srv://vipul:<password>@cluster0.3v2rv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';  {/*9002 ,27017    http://localhost:9002*/}
 export const SKILLS_ENDPOINT = '/api/skills';
 export const PROJECTS_ENDPOINT = '/api/projects';
 export const BLOGS_ENDPOINT = '/api/blogs';
@@ -7,3 +7,9 @@ export const FETCH_SKILLS_SETS = 'FETCH_SKILLS_SETS';
 export const FETCH_PROJECTS = 'FETCH_PROJECTS';
 export const FETCH_BLOGS = 'FETCH_BLOGS';
 
+/*  mongodb://127.0.0.1:27017/portfolio 
+        mongodb+srv://vipul:<password>@cluster0.3v2rv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+        process.env.PORT   `Url`
+        mongodb+srv://cluster0.3v2rv.mongodb.net/myFirstDatabase
+         https://vipulsportfolio.herokuapp.com/
+    */
