72ff168c8099f6f01f5c96f0506cb9257d8dd1ab
vipulgiakwad07 <37871301+vipulgiakwad07@users.noreply.github.com>
2022-04-08 15:41:40 +0530

    Update ContactsComponent.js

--- src/client/components/ContactsComponent.js
+++ src/client/components/ContactsComponent.js
@@ -19,20 +19,30 @@ class ContactsComponent extends React.Component {
         
     }
 
+   
     render() {
         return (
             <div className={'contacts-contents-container'}>
-                 <h4 className={'contacts-footer-number'}>+91 8149 7163 23</h4>
-                 <h4 className={'contacts-emailid'}>vipulgaikwad001@gmail.com</h4>
-                 <Contact />
+             <div className='contact-wrapper'>
+              <div className='mycontacts'> 
+                 <h4 >+91 8149 7163 23</h4>
+                 <h4 >vipulgaikwad001@gmail.com</h4>
+                 
                 <div className={'contacts-social-container'}>
                    {/* <a href="https://www.facebook.com/hackbotone/" target="_blank"><img src={facebook} className={'contact-social-img'}/></a>*/}
                     <a href="https://twitter.com/Vipulgaikwad" target="_blank"><img src={twitter} className={'contact-social-img'}/></a>
                     <a href="https://www.linkedin.com/in/vipul-gaikwad-621833102/" target="_blank"><img src={linkedin} className={'contact-social-img'}/></a>
-                    <a href="https://github.com/vipulgiakwad07" target="_blank"><img src={github} className={'contact-social-img'}/></a>
+                    <a href="https://github.com/anshumanpattnaik" target="_blank"><img src={github} className={'contact-social-img'}/></a>
                    {/* <a href="https://www.youtube.com/anshumanpattnaik" target="_blank"><img src={youtube} className={'contact-social-img'}/></a> */}
                    
                 </div>
+                </div> 
+                        <div>
+                          <Contact />
+                        </div>
+
+                 </div>
+
                 <div className={'contacts-footer-div'}></div>
                 <p className={'contacts-footer-text'}>© 2022 VIPUL GAIKWAD. ALL RIGHTS RESERVED.</p>
             </div>
